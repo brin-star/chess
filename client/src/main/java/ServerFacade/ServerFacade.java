278b000c2dfd0b66a8e10613fcf54d7a457e8f53
Brinley Hancock <brinstar@byu.edu>
2026-03-24 21:29:23 -0600

    makeRequest() works?

--- client/src/main/java/ServerFacade/ServerFacade.java
+++ client/src/main/java/ServerFacade/ServerFacade.java
@@ -1,37 +1,52 @@
 package ServerFacade;
 
+import com.google.gson.Gson;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-import static javax.management.remote.JMXConnectorFactory.connect;
 
 public class ServerFacade {
     private String serverURL = "http://localhost:8080";
 
     private <T> T makeRequest(String method, String endpoint, Object requestBody, String authToken, Class<T> responseClass) throws Exception {
         var url = new URL(serverURL + endpoint);
+        var gson = new Gson();
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod(method);
+        con.setRequestProperty("Content-Type", "application/json");
+
+        if (authToken != null) {
+            con.setRequestProperty("Authorization", authToken);
+        }
 
         if (requestBody != null) {
-            con.connect();
-        }
-        else {
-            con.connect();
+            con.setDoOutput(true);
 
-        }
-        OutputStream os = con.getOutputStream();
-        OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+            String json = gson.toJson(requestBody);
 
-        osw.write(msg.toString());
-        osw.flush();
-        osw.close();
+            OutputStream os = con.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+
+            osw.write(json);
+            osw.flush();
+            osw.close();
+        }
+
+        con.connect();
 
         int responseCode = con.getResponseCode();
         if (responseCode != 200) {
-            throw new Exception("Request failed with code " + responseCode);
+            throw new Exception(con.getErrorStream().toString());
         }
+
+        InputStream is = con.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+
+        T result = gson.fromJson(isr, responseClass);
+        return result;
     }
 }
